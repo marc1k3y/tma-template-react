e0f01ba523846120d46aefa702adda569560fefc
marc1k3y <marc1k3y@gmail.com>
2024-09-24 23:00:32 +0300

    Updates

---
+++ assets/About-BQt8mDqo.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-C5u0BUJa.js";function t(){return e.jsx("div",{children:"about screen"})}export{t as default};
--- assets/About-C-BtPZxq.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DUa7rjkx.js";function t(){return e.jsx("div",{children:"about screen"})}export{t as default};
